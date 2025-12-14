c1120814b66dcc1e6769860bcd3bf56346fc1ab1
RajuTechAssist <oooye.raju@gmail.com>
2025-12-14 12:26:23 +0530

    setup portfolio project structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,40 +1,15 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/common/Header';
-import Footer from './components/common/Footer';
-import { HeroAnimation } from './components/animations/HeroAnimation';
-import { Home } from './pages/Home';
-// import { About } from './pages/About';
-// import { Projects } from './pages/Projects';
-// import { Skills } from './pages/Skills';
-// import { Experience } from './pages/Experience';
-// import { Services } from './pages/Services';
-import './styles/main.css';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      {/* Hero Animation for modern interaction */}
-      <HeroAnimation />
-      
-      <Router>
-        <div className="app-layout">
-          <Header />
-          <main className="main-content">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              {/* <Route path="/about" element={<About />} /> */}
-              {/* <Route path="/projects" element={<Projects />} /> */}
-              {/* <Route path="/skills" element={<Skills />} /> */}
-              {/* <Route path="/experience" element={<Experience />} /> */}
-              {/* <Route path="/services" element={<Services />} /> */}
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </Router>
-    </div>
-  );
+    <main>
+      <div>
+        <h1>
+          Welcome to my 3D Portfolio
+        </h1>
+      </div>
+    </main>
+  )
 }
 
-export default App;
+export default App
